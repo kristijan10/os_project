64ce0829277aa54d29a59874a902895702502b2d
Kristijan <kk220656d@student.etf.bg.ac.rs>
2024-08-03 01:36:25 -0700

    dodate deklaracije i definicije

--- h/syscall_cpp.hpp
+++ h/syscall_cpp.hpp
@@ -2,11 +2,13 @@
 #define syscall_cpp
 
 #include "../lib/hw.h"
-#include "../h/syscall_c.hpp"
+#include "../h/syscall_c.h"
 
+// ============= MEMORIJA =============
 void *operator new(size_t size);
 void operator delete (void *ptr) noexcept;
 
+// ============= NITI =============
 class Thread{
 public:
     Thread(void (*body)(void *), void *arg);
@@ -19,7 +21,7 @@ public:
 
 protected:
     Thread();
-    virtual void run();
+    virtual void run(){}
 
 private:
     thread_t myHandle;
@@ -27,9 +29,10 @@ private:
     void *arg;
 };
 
+// ============= SEMAFOR =============
 class Semaphore{
 public:
-    Semaphore(unsigned init = 1);
+    explicit Semaphore(unsigned init = 1);
     virtual ~Semaphore();
 
     int wait();
@@ -41,18 +44,20 @@ private:
     sem_t myHandle;
 };
 
+// ============= PERIODICNE NITI =============
 class PeriodicThread : public Thread{
 public:
     void terminate();
 
 protected:
-    PeriodicThread(time_t period);
+    explicit PeriodicThread(time_t period);
     virtual void periodicActivation(){}
 
 private:
     time_t period;
 };
 
+// ============= KONZOLA =============
 class Console{
 public:
     static char getc();
--- src/syscall_cpp.cpp
+++ src/syscall_cpp.cpp
@@ -1,6 +1,51 @@
 #include "../h/syscall_cpp.hpp"
 #include "../lib/mem.h"
 
+// ============= MEMORIJA =============
 void *operator new(size_t size) { return __mem_alloc(size); }
 
 void operator delete(void *ptr) noexcept { __mem_free(ptr); }
+
+// ============= NITI =============
+Thread::Thread(void (*body)(void *), void *arg) :
+        myHandle(nullptr),
+        body(body),
+        arg(arg) {
+    thread_create(&myHandle, body, arg);
+}
+
+Thread::~Thread() {}
+
+int Thread::start() { return 0; }
+
+void Thread::dispatch() { thread_dispatch(); }
+
+int Thread::sleep(time_t time) { return time_sleep(time); }
+
+Thread::Thread() :
+        myHandle(nullptr),
+        body(nullptr),
+        arg(nullptr) {}
+
+// ============= SEMAFOR =============
+Semaphore::Semaphore(unsigned int init) : myHandle(nullptr) { sem_open(&myHandle, init); }
+
+Semaphore::~Semaphore() { sem_close(myHandle); }
+
+int Semaphore::wait() { return sem_wait(myHandle); }
+
+int Semaphore::signal() { return sem_signal(myHandle); }
+
+int Semaphore::timedWait(time_t time) { return sem_timedwait(myHandle, time); }
+
+int Semaphore::tryWait() { return sem_trywait(myHandle); }
+
+// ============= PERIODICNE NITI =============
+PeriodicThread::PeriodicThread(time_t period) { this->period = period; }
+
+void PeriodicThread::terminate() { period = 0; }
+
+// ============= KONZOLA =============
+char Console::getc() { return ::getc(); }
+
+void Console::putc(char c) { ::putc(c); }
