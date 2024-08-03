af2ba7c6cc6cfbf32ca340deaed9ca5de1edd972
Kristijan <kk220656d@student.etf.bg.ac.rs>
2024-08-03 01:36:54 -0700

    izmenjen poziv userMain funkcije

--- src/main.cpp
+++ src/main.cpp
@@ -3,31 +3,34 @@
 #include "../h/print.hpp"
 #include "../h/riscv.hpp"
 
+extern void userMain();
+
+void userMainWrapper(void *){
+    userMain();
+}
+
 int main() {
     TCB *threads[5];
 
-    threads[0] = TCB::createThread(nullptr);
+    threads[0] = TCB::createThread(nullptr, nullptr);
     TCB::running = threads[0];
 
-    threads[1] = TCB::createThread(workerBodyA);
-    printStr("ThreadA created!\n");
+    threads[1] = TCB::createThread(userMainWrapper, nullptr);
+    printStr("userMain created!\n");
 
-    threads[2] = TCB::createThread(workerBodyB);
-    printStr("ThreadB created!\n");
-
-    threads[3] = TCB::createThread(workerBodyC);
-    printStr("ThreadC created!\n");
-
-    threads[4] = TCB::createThread(workerBodyD);
-    printStr("ThreadD created!\n");
+//    threads[2] = TCB::createThread(workerBodyB);
+//    printStr("ThreadB created!\n");
+//
+//    threads[3] = TCB::createThread(workerBodyC);
+//    printStr("ThreadC created!\n");
+//
+//    threads[4] = TCB::createThread(workerBodyD);
+//    printStr("ThreadD created!\n");
 
     Riscv::w_sstatus(Riscv::SSTATUS_SIE);
     Riscv::w_stvec((uint64) Riscv::supervisorTrap);
 
-    while (!(threads[1]->isFinished() &&
-             threads[2]->isFinished() &&
-             threads[3]->isFinished() &&
-             threads[4]->isFinished()))
+    while (!(threads[1]->isFinished()))
         TCB::yield();
 
     for (auto &thread: threads) {
